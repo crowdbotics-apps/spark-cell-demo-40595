88468dc0fef6ec715e00043f0b4cddc917bd0c55
Crowdbotics <team@crowdbotics.com>
2023-05-10 01:43:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spark-cell-demo-40595",
+  url: "https://spark-cell-demo-40595.botics.co",
+  api: "https://spark-cell-demo-40595.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
